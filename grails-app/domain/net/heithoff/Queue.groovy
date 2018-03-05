ac211d82a56f25662c65f64d7f8fef8749c59ce8
Jason Heithoff <jheithof@fau.edu>
2018-03-05 18:09:21 -0500

    intial commit

---
+++ grails-app/domain/net/heithoff/Queue.groovy
@@ -0,0 +1,14 @@
+package net.heithoff
+
+class Queue {
+
+    static constraints = {
+    }
+
+    String firstName
+    String lastName
+    String contact
+
+    Date dateCreated
+    Date lastUpdated
+}
